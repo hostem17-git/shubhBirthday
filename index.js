f2981b34463a7b06d93df345add6db96102e150a
hostem17-git <vjjais17@gmail.com>
2021-05-16 23:05:51 +0530

    first Commit

---
+++ index.js
@@ -0,0 +1,119 @@
+var canvas = document.querySelector("canvas");
+canvas.width = innerWidth;
+canvas.height = innerHeight;
+
+var sound = new Audio("cracker.mp3")
+var ctx = canvas.getContext("2d");
+var mouse={
+    x:undefined,
+    y:undefined
+}
+
+
+addEventListener("resize",function(){
+    canvas.width = innerWidth;
+    canvas.height = innerHeight;
+});
+
+function getRandomColor(){
+    var colors =['#f21170','#72147e','#fa9905',"#ff5200",'#7b113a','#150e56','#1597bb','#8fd6e1','#72147e','#f21170','#fa9905','#ff5200','#fb3640','#542e71','#fdca40'];
+    return colors[Math.floor(Math.random()*colors.length)]; 
+}
+
+
+var gravity = 0.03;
+var friction = .99;
+
+class Particle{
+    constructor(x,y,radius,color,velocity){
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+        this.color = color;
+        this.velocity = {
+            x:velocity.x,
+            y:velocity.y
+        }
+        this.opacity = 1;
+    }
+
+    draw(){
+        ctx.save()
+        ctx.globalAlpha =this.opacity;
+
+        ctx.beginPath();
+        ctx.arc(this.x,this.y,this.radius,0,2*Math.PI,false);
+        ctx.fillStyle = this.color;
+        ctx.fill();
+        ctx.closePath();
+        ctx.restore();
+    }
+
+    update(){
+        this.draw();
+        this.velocity.x *= friction;
+        this.velocity.y *= friction;
+        this.velocity.y += gravity;
+        this.x += this.velocity.x;
+        this.y += this.velocity.y;
+        this.opacity-=0.003;
+        
+    }
+}
+
+var particles;
+
+function init(){
+    particles=[];
+}
+
+
+var particleSpeed = 12;
+addEventListener("click",(event)=>{
+    mouse.x = event.clientX;
+    mouse.y = event.clientY;
+    sound.play();
+    var particleCount = 500;
+    var angleIncrement = Math.PI * 2 / particleCount;
+    
+    for(var i=0;i<particleCount;i++){
+        particles.push( new Particle(mouse.x,mouse.y,5,getRandomColor(),{
+            x:  Math.cos(angleIncrement*i) *Math.random()*particleSpeed,
+            y: Math.sin(angleIncrement*i)*Math.random()*particleSpeed
+                }
+            )
+        );
+    }
+})
+
+var imge = new Image();
+imge.src = "shubh.jpg";
+var flag = false;
+
+
+function animate(){
+    requestAnimationFrame(animate);
+    if(!flag)
+        {imge.onload =function(){
+            flag = true;
+        }
+        }
+    ctx.fillStyle='rgba(0,0,0,.05)'
+    ctx.fillRect(0,0,canvas.width,canvas.height);
+    particles.forEach((particle, i) => {
+        if (particle.opacity > 0) {
+          particle.update()
+        } else {
+          particles.splice(i, 1)
+        }
+      })
+    ctx.font= "90px Dancing Script"
+    ctx.fillStyle = "rgb(255,255,255)";
+    ctx.fillText("Happy Birthweek Shubh!", 330,200)
+    if(flag)
+        ctx.drawImage(imge,canvas.width/2 - 150,canvas.height/2 - 100,300,300)
+}
+
+
+init();
+animate();
